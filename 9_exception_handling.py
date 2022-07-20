098e259e71a6698195b709b2030a2d8a44d32234
unknown <bigbosshong1101@gmail.com>
2022-07-20 20:47:42 +0900

    python exception handling

---
+++ 9_exception_handling.py
@@ -0,0 +1,28 @@
+chicken = 10
+waiting = 1
+
+
+class SoldOutError(Exception):
+    pass
+
+while(True):
+    try:
+        print("[남은 치킨: {0}]".format(chicken))
+        order = int(input("치킨 몇마리 주문하시겠습니까?"))
+        
+        if order > chicken:
+            print("재료가 부족합니다.")
+        elif order < 1:
+            raise ValueError    
+        else:
+            print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+            waiting += 1
+            chicken -= order
+
+        if chicken == 0:
+            raise SoldOutError
+    except ValueError:
+        print('잘못된 값을 입력하셨습니다.')
+    except SoldOutError:
+        print("재료가 소진되어서 더 이상 주문을 받지 않았습니다.")
+        break
